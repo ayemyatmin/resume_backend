336554ba5d0f5d76e2a4e77fb1ab71b949caec0c
ayemyatmin <ayemyatmin9999@gmail.com>
2026-06-09 15:05:50 +1000

    Feat: Increments the visitorCount in dyanmodb Visitor table when this endpoint is called

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-export async function handler(event) {
-  // Lambda function logic here
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-    message: "Hello from Lambda!"
-  })
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event) => {
+  try {
+    const command = new UpdateCommand({
+      TableName: "Visitor",
+      Key: {
+        statId: "visitorCount"
+      },
+      UpdateExpression: "ADD visitorCount :inc",
+      ExpressionAttributeValues: {
+        ":inc": 1
+      },
+      ReturnValues: "ALL_NEW"
+    });
+
+    const result = await docClient.send(command);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        visitorCount: result.Attributes.visitorCount
+      })
+    };
+
+  } catch (error) {
+    console.error("Error updating visitor count:", error);
+
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
 };
-
-return response;
-}
